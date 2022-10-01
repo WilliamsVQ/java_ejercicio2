c1d41ba731020031978567d0c8095d0dc0dfa670
corvuspdg <williamsvq254@gmail.com>
2022-09-30 22:21:26 -0400

    some init msg

---
+++ src/com/OpenBC/ejecicio2.java
@@ -0,0 +1,23 @@
+package com.OpenBC;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+
+public class ejecicio2 {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Introducir precio");
+        double a = leer.nextDouble();
+        System.out.println("El precio con IVA es:");
+        DecimalFormat b = new DecimalFormat("#.##");
+        a = Double.valueOf(b.format(precio(a)));
+        System.out.println(a);
+    }
+    public static double precio(double a){
+        a = a + (a*21/100);
+        return a;
+    }
+}
+
+
+
